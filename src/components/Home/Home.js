805100c553341f885cbbaa7bce129468c9ce06fe
aks2042 <freelancerakash3@gmail.com>
2021-05-28 23:41:43 +0600

    home add lorem

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,6 +4,7 @@ const Home = () => {
     return (
         <div>
             <h1>Home Component</h1>
+            <h2>lorem</h2>
             <p>Lorem ipsum dolor sit amet.</p>
         </div>
     );
